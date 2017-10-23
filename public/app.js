ed8b772054da74a39f38da02d07c5b7541079b55
valbeat <kj1ktk@gmail.com>
2017-10-23 22:38:00 +0900

    p37. ハッシュによってViewを切り替える

--- public/app.js
+++ public/app.js
@@ -1,6 +1,17 @@
 'use strict'
+
 var learnjs = {};
+
+learnjs.problemView = function() {
+    return $('<div class="problem-view">').text('Comming soon!');
+};
+
 learnjs.showView = function(hash) {
-  var problemView = $('<div class="problem-view">').text('Comming soon!');
-  $('.view-container').empty().append(problemView);
-}
+    var routes = {
+        '#problem-1' : learnjs.problemView
+    };
+    var viewFn = routes[hash];
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn());
+    }
+};
