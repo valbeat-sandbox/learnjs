0b4e12246afaf905bcc7a9d931132b6cbc806d99
valbeat <kj1ktk@gmail.com>
2017-10-31 03:31:13 +0900

    p45. appOnReady内でshowViewをhashつきでcallするのでテストが通る

--- public/app.js
+++ public/app.js
@@ -2,6 +2,10 @@
 
 var learnjs = {};
 
+learnjs.appOnReady = function () {
+    learnjs.showView(window.location.hash);
+};
+
 learnjs.problemView = function(problemNumber) {
     var title = 'Problem #' + problemNumber + ' Coming soon!';
     return $('<div class="problem-view">').text(title);
