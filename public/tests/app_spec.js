f228f7cedff716b2ad5b9f14f50250bf59193cb1
valbeat <kj1ktk@gmail.com>
2017-10-31 02:48:48 +0900

    テスト追加

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,15 +1,22 @@
 describe('LearnJS', function() {
-  it('can show a problem view', function(){
-    learnjs.showView('#problem-1');
-    expect($('.view-container .problem-view').length).toEqual(1);
-  });
-  it('show the landing page view when there is no hash', function(){
-    learnjs.showView('');
-    expect($('.view-container .landing-view').length).toEqual(1);
-  });
-  it('passes the hash view parameter to the view function', function(){
-    spyOn(learnjs, 'problemView');
-    learnjs.showView('#problem-42');
-    expect(learnjs.problemView).toHaveBeenCalledWith('42');
-  });
+    it('can show a problem view', function(){
+        learnjs.showView('#problem-1');
+        expect($('.view-container .problem-view').length).toEqual(1);
+    });
+    it('show the landing page view when there is no hash', function(){
+        learnjs.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
+    });
+    it('passes the hash view parameter to the view function', function(){
+        spyOn(learnjs, 'problemView');
+        learnjs.showView('#problem-42');
+        expect(learnjs.problemView).toHaveBeenCalledWith('42');
+    });
+});
+
+describe('problem View',function () {
+    it('has a title that include the problem number', function(){
+        var view = learnjs.problemView('1');
+        expect(view.text()).toEqual('Problem #1 Coming soon!');
+    });
 });
