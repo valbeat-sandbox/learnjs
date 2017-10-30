27f5ab0935943da4ecf591ecb9148204527ce5cb
valbeat <kj1ktk@gmail.com>
2017-10-31 04:53:50 +0900

    p58. add data model

--- public/app.js
+++ public/app.js
@@ -2,6 +2,17 @@
 
 var learnjs = {};
 
+learnjs.problems = [
+    {
+        description: "What is truth?",
+        code: "function problem() { return __; }"
+    },
+    {
+        description: "Simple Math",
+        code: "function problem() { return 42 === 6 * __; }"
+    }
+];
+
 learnjs.appOnReady = function () {
     // attach listener to window object's onhashchange event
     window.onhashchange = function() {
@@ -23,9 +34,17 @@ learnjs.showView = function(hash) {
     }
 };
 
-learnjs.problemView = function(problemNumber) {
+learnjs.problemView = function(data) {
+    var problemNumber = parseInt(data, 10);
     var view = $('.templates .problem-view').clone();
     var title = 'Problem #' + problemNumber + ' Coming soon!';
     view.find('.title').text(title);
+    learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
     return view;
+};
+
+learnjs.applyObject = function(obj, elm) {
+    for (var key in obj) {
+        elm.find('[data-name="'+ key + '"]').text(obj[key]);
+    }
 };
