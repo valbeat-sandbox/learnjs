9dc374cdf26feb4885a500f5f29609403acbc87d
valbeat <kj1ktk@gmail.com>
2017-11-04 07:24:11 +0900

    p58. add format spec

--- public/tests/format_spec.js
+++ public/tests/format_spec.js
@@ -1,1 +1,22 @@
-return learnjs.problems.map(learnjs.formatCode);
+learnjs.formatCode = function (obj) {
+    return obj;
+};
+
+function formatProblems() {
+    return learnjs.problems.map(learnjs.formatCode);
+}
+
+describe('formatProblems', function () {
+    beforeEach(function () {
+        spyOn(learnjs, 'formatCode').and.callFake(function(problem){
+            return {
+                code: 'formatted',
+                name: problem.name
+            };
+        });
+    });
+
+    it('aplies a formatter to all the problems', function () {
+        expect(formatProblems()[0].code).toEqual('formatted');
+    });
+});
